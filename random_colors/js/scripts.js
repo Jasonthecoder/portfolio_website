3f54ff0a62186f266802cc1a9a143ef2d336f8e1
Matthew Pagan <mpagan@MatthewPagan.com>
2016-04-11 16:27:38 -0400

    Changed the UI for the random color generator. Uses buttons instead of ranges. Sloppy code though. Will refactor in next commit.

--- random_colors/js/scripts.js
+++ random_colors/js/scripts.js
@@ -1,34 +1,176 @@
-var colorProperties = {};
+(function(){
 
-function createColors() {
-  var html = "";
+var
+  defaultProps = {
+    minHue: 0,
+    maxHue: 360,
+    minSaturation: 40,
+    maxSaturation: 70,
+    minLightness: 40,
+    maxLightness: 70
+  },
+  warmProps = {
+    minHue: -10,
+    maxHue: 70,
+    minSaturation: 40,
+    maxSaturation: 70,
+    minLightness: 40,
+    maxLightness: 70
+  },
+  coolProps = {
+    minHue: 120,
+    maxHue: 260,
+    minSaturation: 40,
+    maxSaturation: 70,
+    minLightness: 40,
+    maxLightness: 70
+  },
+  currentSetting = defaultProps;
 
-  var minHue = document.getElementById("minHue");
-  colorProperties["minHue"] = parseInt(minHue.options[minHue.selectedIndex].value);
+  function createColors(colorProp) {
+    var html = "";
+    var colorObject;
+    currentSetting = colorProp;
+    for ( var i = 1; i <= 100; i++ ) {
+      colorObject = new Color(currentSetting);
+      colorHTML = colorObject.toHTML()
+      html += "<div class='number' style='background: " + colorHTML + "; '>" + i + "</div>";
+    }
+    $("#colorOutput").html(html);
+  }  
+
+  $(createColors);
   
-  var maxHue = document.getElementById("maxHue");
-  colorProperties["maxHue"] = parseInt(maxHue.options[maxHue.selectedIndex].value);
+  $("#all_lightest").on("click", function(){
+    createColors({
+      minHue: 0,
+      maxHue: 360,
+      minSaturation: 40,
+      maxSaturation: 70,
+      minLightness: 70,
+      maxLightness: 90
+    });
+  });
+  $("#all_lighter").on("click", function(){
+    createColors({
+      minHue: 0,
+      maxHue: 360,
+      minSaturation: 40,
+      maxSaturation: 70,
+      minLightness: 60,
+      maxLightness: 80
+    });
+  });
+  $("#all").on("click", function(){
+    createColors(defaultProps);
+  });
+  $("#all_darker").on("click", function(){
+    createColors({
+      minHue: 0,
+      maxHue: 360,
+      minSaturation: 40,
+      maxSaturation: 70,
+      minLightness: 30,
+      maxLightness: 50
+    });
+  });
+  $("#all_darkest").on("click", function(){
+    createColors({
+      minHue: 0,
+      maxHue: 360,
+      minSaturation: 40,
+      maxSaturation: 70,
+      minLightness: 20,
+      maxLightness: 30
+    });
+  });
 
-  var minSaturation = document.getElementById("minSaturation");
-  colorProperties["minSaturation"] = parseInt(minSaturation.options[minSaturation.selectedIndex].value);
+  $("#warm_lightest").on("click", function(){
+    createColors({
+      minHue: -10,
+      maxHue: 70,
+      minSaturation: 40,
+      maxSaturation: 70,
+      minLightness: 70,
+      maxLightness: 90
+    });
+  });
+  $("#warm_lighter").on("click", function(){
+    createColors({
+      minHue: -10,
+      maxHue: 70,
+      minSaturation: 40,
+      maxSaturation: 70,
+      minLightness: 60,
+      maxLightness: 80
+    });
+  });
+  $("#warm").on("click", function(){
+    createColors(warmProps);
+  });
+  $("#warm_darker").on("click", function(){
+    createColors({
+      minHue: -10,
+      maxHue: 70,
+      minSaturation: 40,
+      maxSaturation: 70,
+      minLightness: 30,
+      maxLightness: 50
+    });
+  });
+  $("#warm_darkest").on("click", function(){
+    createColors({
+      minHue: -10,
+      maxHue: 70,
+      minSaturation: 40,
+      maxSaturation: 70,
+      minLightness: 20,
+      maxLightness: 30
+    });
+  });
 
-  var maxSaturation = document.getElementById("maxSaturation");
-  colorProperties["maxSaturation"] = parseInt(maxSaturation.options[maxSaturation.selectedIndex].value);
+  $("#cool_lightest").on("click", function(){
+    createColors({
+      minHue: 120,
+      maxHue: 260,
+      minSaturation: 40,
+      maxSaturation: 70,
+      minLightness: 70,
+      maxLightness: 90
+    });
+  });
+  $("#cool_lighter").on("click", function(){
+    createColors({
+      minHue: 120,
+      maxHue: 260,
+      minSaturation: 40,
+      maxSaturation: 70,
+      minLightness: 60,
+      maxLightness: 80
+    });
+  });
+  $("#cool").on("click", function(){
+    createColors(coolProps);
+  });
+  $("#cool_darker").on("click", function(){
+    createColors({
+      minHue: 120,
+      maxHue: 260,
+      minSaturation: 40,
+      maxSaturation: 70,
+      minLightness: 30,
+      maxLightness: 50
+    });
+  });
+  $("#cool_darkest").on("click", function(){
+    createColors({
+      minHue: 120,
+      maxHue: 260,
+      minSaturation: 40,
+      maxSaturation: 70,
+      minLightness: 20,
+      maxLightness: 30
+    });
+  });
 
-  var minLightness = document.getElementById("minLightness");
-  colorProperties["minLightness"] = parseInt(minLightness.options[minLightness.selectedIndex].value);
-
-  var maxLightness = document.getElementById("maxLightness");
-  colorProperties["maxLightness"] = parseInt(maxLightness.options[maxLightness.selectedIndex].value);
-
-  (document.documentElement.clientWidth < 800) ? j = 25 : j = 100;
-  for (var i = 1; i <= j; i++) {
-    var colorObject = new Color(colorProperties);
-    var colorHTML = colorObject.toHTML();
-    html += "<div class='number' style='background: " + colorHTML + "'>" + i + "</div>"
-    var colorDiv = document.getElementById("colorOutput");
-    colorDiv.innerHTML = html;
-  }
-}
-
-createColors();
+})();
